b2584c38bdace13fd7dba12f22f4a9eef9ad6554
GennadiiTushinskii <genadyus@mail.ru>
2023-09-14 15:31:25 +0300

    Task2-9

--- Task9/src/Square.java
+++ Task9/src/Square.java
@@ -4,16 +4,16 @@ public class Square extends Quadrangle implements Figure{
     }
 
     public double getArea() {
-        return a*b;
+        return a * a;
     }
 
     public double getPerimeter() {
-        return 2*a+2*b;
+        return 4 * a;
     }
 
     @Override
     double getLargeDiagonal() {
-        return a*Math.sqrt(2);
+        return a * Math.sqrt(2);
     }
 
     @Override
